f73a56c4b1f8057fdebe40a8e4689163a949ed9b
fareed gul <fareedgul735@gmail.com>
2025-03-28 16:33:33 +0500

    add my changing for my file

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-// clg//
+// clg //
 // console.log("hello iam console !");
-// clg//
+// clg //
 
 // Alerts //
 
-// alert("hello user");
+// let alerting = alert("hello user");
+// console.log(alerting);
 
 // var confirmed = confirm("Are You Muslim");
 // console.log(confirmed);
@@ -132,3 +133,157 @@
 // let 123IlegalVar = "hey iam ileagl var !";
 // let Ilegal-Var = "hey iam ileagl var !"; is is ileagl var
 // console.log(123IlegalVar); this is ileagl var
+
+// Math Experssion Familiar Operators //
+
+// + // addition
+// - // subtraction
+// * // multiplication
+// / // division
+// % //  modulus
+
+// let num = 9;
+// let num2 = 18;
+
+// console.log(num + num2); // this is sum operator //
+
+// let subtract = 65;
+// let subtract2 = 54;
+// let result = subtract - subtract2;
+// console.log(result); // this is sub operator
+
+// let multi = 4 * 4 ;
+// console.log(multi); this is multiplication
+
+// let expo = 10 * 10 * 10;
+// console.log(expo); // this is exponantion
+
+// let divide = 2 / 5;
+// console.log(divide); // this is division
+
+// let modulus = 2 % 5;
+// console.log(modulus); //this is modulus
+
+// Math Experssion UnFamiliar Operators //
+
+// let num = 2;
+// num++; // this is a post increment // output 3
+// num--; // this is a post decrement // output 2
+// console.log(num); // output 2
+
+// let num = 4;
+// --num; // this is a pre decrement // output 3 //
+// ++num; // this is a pre increment // output 4 //
+// console.log(num);
+
+// Math Expressions: eliminating ambiguity //
+
+// (BODMAS) // Border/Division/Mutiplication/Addition/Subtraction
+
+// let number = 2 + 4 * 5;
+// console.log(number); // this is a bodmas // output 22
+
+// let number = (2 + 4) * 5;
+// console.log(number); // output 30 //
+
+// Concatenating text strings //
+
+// let firstName = "fareed";
+// let lastName = " gul";
+
+// console.log(firstName + lastName); // this is a concatenate
+
+// let dummyText = " Thanks";
+// let dummyText2 = " For Watching";
+// let dummyText3 = " !";
+
+// let resultOfDummyText = dummyText + dummyText2 + dummyText3; // this is a concatenate
+// console.log(resultOfDummyText);
+
+// Prompts //
+
+// let userCheck = prompt("enter your name");
+// alert("Hello " + userCheck);
+
+// let user = prompt("enter your name !");
+// let userAge = +prompt("enter your age !");
+
+// alert("userName " + user + "userAge " + userAge);
+
+// if  else statements //
+
+// let userAge = +prompt("enter your age !");
+
+// if (userAge > 18 && userAge < 32) { // yaha agar condition true hogi tu if statement chal jayegi
+//   alert("yes you are eligible of course");
+// } else {
+//   alert("sorry you are not a eligible of course !"); // or yahan pr agar if condition false hogi else statement chal jayegi
+// }
+
+// if  else statements //
+
+// Comparison Operators //
+
+// console.log(5 == "5"); // true // value same honi chaye hai bss datatype ahmiyat nhi rakhta ||
+// console.log(5 === "5"); // false // value or datatype dono same honi chaye hai ||
+// console.log(5 > 6); // false // pehle value dosri value say bari honi chaye hai ||
+// console.log(5 < 6); // true // pehle value dosri value say choti honi chaye hai ||
+// console.log(5 !== 5); // false // ya value same nhi ho || ya pr datatype same nhi  ho ||
+// console.log(5 != "5"); // false // value same nhi honi chaye datatype ahmiyat nhi rakhta ||
+
+// Comparison Operators //
+
+// if else if //
+
+// let userCheck = prompt("pakistan big city name !");
+
+// if (userCheck === "karachi") {
+//   alert("correct answer");
+//   confirm("congrats you are winner 😊");
+// } else if (userCheck === "") {
+//   alert("please fill the field !");
+// } else if (userCheck === "peshawar") {
+//   alert("sorry you are close but wrong answer !");
+//   confirm("best try but try again !");
+// } else if (userCheck === "lahore") {
+//   alert("sorry you are ver close but wrong answer !");
+//   confirm("best try but try again !");
+// } else {
+//   alert("wrong answer !");
+// }
+
+// if else if //
+
+// Logical Operators //
+
+// console.log(5 > 4 && 5 < 7); // true // yaha and operator use kiya hai jho dono condition true mangta hai ||
+// console.log(6 > 10 || 9 < 6); // false // yaha pr ek condition ka true hona zarori hai ||
+// console.log(!(5 < 9)); // false // yai opposite return karta hai true ko false or false true kardeta hai ulat ||
+
+// Logical Operators //
+
+// if statement nested //
+
+// let userAge = +prompt("Enter your age!");
+
+// if (userAge >= 18) {
+//   if (userAge <= 50) {
+//     alert("You are an adult.");
+//   } else {
+//     alert("You are a senior citizen.");
+//   }
+// } else {
+//   alert("You are a child.");
+// }
+
+// if statement nested //
+
+// ternary Operators //
+
+// let userAge = +prompt("enter your age for course!");
+
+// let userData = userAge >= 18 ? "your allowed" : "your not allowed";
+
+// alert(userData);
+
+// ternary Operators //
